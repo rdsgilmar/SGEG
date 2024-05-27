0da1321f65bbaaa053cacf54c0eedfa7d5510fee
Gilmar <gilmarrds93@gmail.com>
2024-05-27 20:03:56 -0300

    front

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
-// Importar o middleware de autorização
-const authorizeSchool = require('../middlewares/authorizeSchool');
-//
-const dashboardController = require('../controllers/dashboardController');
 
 // Rota de inicialização do sistema
 router.get('/', (req, res) => {
@@ -12,27 +7,20 @@ router.get('/', (req, res) => {
     res.render('pages/login');
 });
 
-// Rotas de autenticação
-router.get('/register', authController.renderRegisterPage);
-router.post('/register', authController.register);
-router.post('/login', authController.login); // Nova rota para login
-
-// // Rota para a página de destino (dashboard)
-// router.get('/dashboard', (req, res) => {
-//     res.render('pages/dashboard');
-// });
-
-
-
-// Rota protegida
-router.get('/dashboard/:schoolId', authorizeSchool, (req, res) => {
-    // Aqui você pode acessar os dados da escola correspondente
-    // A lógica de acesso aos dados da escola deve ser implementada aqui
-    res.json({ message: 'Bem-vindo ao dashboard da escola.' });
+router.get('/cadastro', (req, res) => {
+    console.log('Rota do cadastro acessada.');
+    res.render('pages/cadastro');
 });
 
-// Rota para renderizar o dashboard
-router.get('/dashboard', dashboardController.renderDashboard);
+router.get('/loading', (req, res) => {
+    console.log('Rota do loading acessada.');
+    res.render('pages/loading');
+});
+
+router.get('/dashboard', (req, res) => {
+    console.log('Rota do dashboard acessada.');
+    res.render('pages/dashboard');
+});
 
 // Exportando módulo router
 module.exports = router;
